e55ae5d5144a8e64e1f29f6161234d589b070bf0
Todd Stellanova <tstellanova@users.noreply.github.com>
2022-10-23 11:17:37 -0700

    update examples for new HAL version

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -12,7 +12,6 @@ use rt::entry;
 use panic_rtt_core::{self, rprintln, rtt_init_print};
 
 use embedded_hal::blocking::delay::DelayMs;
-use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
 use stm32f401ccu6_bsp::peripherals;
 
 
@@ -21,8 +20,13 @@ fn main() -> ! {
     rtt_init_print!(NoBlockTrim);
     rprintln!("--> MAIN --");
 
-    let (mut user_led, mut delay_source, _i2c1_port, _spi1_port, _csn_pin) =
-        peripherals::setup_peripherals();
+    let (mut user_led,
+        mut delay_source,
+        _i2c1_port,
+        _spi1_port,
+        _csn_pin,
+        mut _timeout_timer
+    ) = peripherals::setup_peripherals();
 
     let _ = user_led.set_high();
 
